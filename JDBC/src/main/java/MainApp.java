b6ef34156bfc1d6edeb229f7d10994cee059fd7c
Anonymous <user@rivne.itstep.org>
2020-02-01 11:23:18 +0200

    something-wrong

--- JDBC/src/main/java/MainApp.java
+++ JDBC/src/main/java/MainApp.java
@@ -1,20 +1,13 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.SQLException;
 
 public class MainApp {
+
 	public static void main(String[] args) {
 
 		try {
-			String hostName = "10.7.24.196";
-			String dbName = "paraskadb";
-			String userName = "vova";
-			String password = "Qwerty1-";
-
-			Class.forName("org.postgresql.Driver");
-
-			Connection conn = DriverManager.getConnection("jdbc:postgresql://" + hostName + ":5432/" + dbName, userName,
-					password);
+			Connection conn = GetConnection();
 			if (conn != null) {
 				System.out.println("Підключення успішне");
 			} else {
@@ -25,5 +18,18 @@ public class MainApp {
 			System.out.println("У нас проблеми " + e.getMessage());
 		}
 
+	}
+
+	private static Connection GetConnection() throws SQLException, ClassNotFoundException {
+		String hostName = "10.7.24.196";
+		String dbName = "paraskadb";
+		String userName = "vova";
+		String password = "Qwerty1-";
+
+		Class.forName("org.postgresql.Driver");
+
+		Connection conn = DriverManager.getConnection("jdbc:postgresql://" + hostName + ":5432/" + dbName, userName,
+				password);
+		return conn;
 	}
 }
